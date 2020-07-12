1fba0d4a242e85319b76c254461c06b499fa8893
Robin Philip <rbnphlp@hotmail.com>
2020-07-12 11:22:35 +0000

    Procfile added

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def hello():
     return ("Hello world again")
 
 
-if  __name__=="__main__":
+if  __name__=="__main__" :
     app.run(host=os.environ.get("IP","0.0.0.0"),
-    port=int(os.environ.get("PORT","5000")),debug=True)
+    port = int(os.environ.get("PORT","5000")),debug=True)
 
