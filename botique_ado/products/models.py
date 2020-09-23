23c7e61dc8555f45731856a3b7436b21015f661f
Robin Philip <rbnphlp@hotmail.com>
2020-09-23 22:30:28 +0000

    customised admin

--- botique_ado/products/models.py
+++ botique_ado/products/models.py
@@ -4,6 +4,12 @@ from django.db import models
 
 
 class Category(models.Model):
+       
+
+       class Meta:
+            verbose_name_plural='Categories'
+
+            
        name=models.CharField(max_length=254,null=True,blank=True)
        friendly_name=models.CharField(max_length=254,null=True,blank=True)
 
