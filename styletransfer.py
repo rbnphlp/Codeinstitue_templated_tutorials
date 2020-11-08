22d906ecbf0be677876d2edfa42eb8b40d6d5e2c
Robin Philip <rbnphlp@hotmail.com>
2020-11-08 20:42:09 +0000

    add code to combine pictures vertically

--- styletransfer.py
+++ styletransfer.py
@@ -12,6 +12,26 @@ import time
 import functools
 
 
+new_width  = 380
+new_height = 512
+
+def get_concat_v_resize(im1, im2, resample=PIL.Image.BICUBIC, resize_big_image=True):
+    if im1.width == im2.width:
+        _im1 = im1
+        _im2 = im2
+    elif (((im1.width > im2.width) and resize_big_image) or
+          ((im1.width < im2.width) and not resize_big_image)):
+        _im1 = im1.resize((im2.width, int(im1.height * im2.width / im1.width)), resample=resample)
+        _im2 = im2
+    else:
+        _im1 = im1
+        _im2 = im2.resize((im1.width, int(im2.height * im1.width / im2.width)), resample=resample)
+    dst = PIL.Image.new('RGB', (_im1.width, _im1.height + _im2.height))
+    dst.paste(_im1, (0, 0))
+    dst.paste(_im2, (0, _im1.height))
+    return dst
+
+
 def tensor_to_image(tensor):
   tensor = tensor*255
   tensor = np.array(tensor, dtype=np.uint8)
@@ -60,12 +80,21 @@ for files in onlyfiles:
     
     file_=mypath+"/"+files
     content_path = file_
-    style_path = "style_pics/human_Style.jpeg"
+    style_path = "style_pics/red_style.jpeg"
 
 
 
     content_image = load_img(content_path)
     style_image = load_img(style_path)
+
+    "load images "
+    content_image_pil = PIL.Image.open(content_path)
+    style_image_pil = PIL.Image.open(style_path)
+    "Combine the style and upload Pic togetger "
+    combined_image=get_concat_v_resize(content_image_pil, style_image_pil, resize_big_image=False)
+    combined_image=combined_image.resize((new_width, new_height), PIL.Image.ANTIALIAS)
+    combined_image.save('combined'+"/"+'red_style'+files)
+    
 
 
 
@@ -75,8 +104,7 @@ for files in onlyfiles:
     l=tensor_to_image(stylized_image)
 
     # Resize image
-    new_width  = 380
-    new_height = 512
+   
     l= l.resize((new_width, new_height), PIL.Image.ANTIALIAS)
 
     # creating a image object (main image)  
